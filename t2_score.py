66437887bb1b8c2de47ad9b43913f6ae6a507a10
Israel Saeta Pérez <dukebody@gmail.com>
2016-06-06 19:58:04 +0200

    Add script to get map5 score over t2.csv.

---
+++ t2_score.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import ml_metrics
+
+
+real = pd.read_csv('t2.csv')
+predictions = pd.read_csv('submission_last.csv')
+
+# predictions are in format " c1 c2 c3 c4 c5"
+clusters_pred = predictions['hotel_cluster'].str.strip().str.split(' ')
+# need to cast to str bc predictions are str as well
+clusters_real = [[str(l)] for l in real["hotel_cluster"]]
+
+print(ml_metrics.mapk(clusters_pred, clusters_real, k=5))
